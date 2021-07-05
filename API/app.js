7d8aa34e00d0c48fab21b212c55bf8a22ce3849f
MrPacMan1 <33896105+MrPacMan1@users.noreply.github.com>
2021-07-05 20:22:59 +0300

    added sessions and cookies functionality

--- API/app.js
+++ API/app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const usersRoute = require('./routes/users');
 const bcrypt = require('bcrypt')
 var cors = require('cors');
+const session = require('express-session');
+
+const MongoStore = require('connect-mongo');
 
 
 app = express();
@@ -12,9 +15,29 @@ app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api/users', usersRoute);
-mongoose.connect(process.env.DB_CONNECT,
+
+
+const connect = mongoose.connect(process.env.DB_CONNECT,
                 {useNewUrlParser: true, useUnifiedTopology: true});
+                
+// const sessionStore = new MongoStore({
+//     mongooseConnection: mongoose.connection,
+//     collection: 'sessions'
+// });
+
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({mongoUrl: process.env.DB_CONNECT}),
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 //1 day
+    }
+}));
+
+app.use('/api/users', usersRoute);
 
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+});
